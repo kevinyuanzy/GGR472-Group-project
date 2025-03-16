f6a481646f2404c9b3734a743c8b2ce21cd390b0
kevinyuanzy <yuansirzy126@gmail.com>
2025-03-16 15:34:15 -0400

    added point file

--- script.js
+++ script.js
@@ -6,6 +6,54 @@ const map = new mapboxgl.Map({
     container: 'Lab2Map', // map container ID in the index.html file.
     style: 'mapbox://styles/kevinyuanzy/cm6ztbpqc003s01qwcvdrf6ft', // style URL from created MapBox style.
     center: [-79.391820, 43.701268], // starting position [lng, lat]. 
-    zoom: 11.25, // starting zoom level.
+    zoom: 11, // starting zoom level.
 });
 
+map.on('load', () => {
+    map.addSource('signature_sites', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/kevinyuanzy/GGR472-Group-project/refs/heads/main/data/Toronto%20Signature%20Sites%20-%204326.geojson' // The URL to GeoJson completed portion of subway line.
+    });
+
+    map.addLayer({
+        'id': 'toronto-signature-sites-points', 
+        'type': 'circle', 
+        'source': 'signature_sites',
+        'paint': {
+            'circle-color': '#ff1a1a',
+            'circle-radius': 2 
+        },
+    });
+
+    map.addSource('police_facilities', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/kevinyuanzy/GGR472-Group-project/refs/heads/main/data/Police_Facilities_-8924748313065318893.geojson' // The URL to GeoJson completed portion of subway line.
+    });
+
+    map.addLayer({
+        'id': 'toronto-police-facilities-points',
+        'type': 'circle',
+        'source': 'police_facilities',
+        'paint': {
+            'circle-color': '#105694',
+            'circle-radius': 2 
+        },
+    });
+
+    map.addSource('community_housing', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/kevinyuanzy/GGR472-Group-project/refs/heads/main/data/Community%20Housing%20Data%20-%204326.geojson' // The URL to GeoJson completed portion of subway line.
+    });
+
+    map.addLayer({
+        'id': 'toronto-community-housing-points',
+        'type': 'circle',
+        'source': 'community_housing',
+        'paint': {
+            'circle-color': '#edc707',
+            'circle-radius': 2 
+        },
+    });
+
+
+})
