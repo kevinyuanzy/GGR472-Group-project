ee7362b308eacdf8f0390076773cb671fa616a01
huailun-j <huailun.jiang@mail.utoronto.ca>
2025-03-16 16:44:15 -0400

    add

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Add the default map token from the Mapbox account
-mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW55dWFuenkiLCJhIjoiY201eHprYXU0MGZwejJsb242Y3Nza25oYyJ9.h05hqdnqlx2BwgwbQNuKCg'; 
+mapboxgl.accessToken = 'pk.eyJ1IjoieGlleWl3ZTIiLCJhIjoiY201bzlrMzF4MGttMTJub20xODk5dGxydiJ9._U9znMhQu-2lUtT3MidkQg'; 
 
 //Import the map style from MapBox. 
 const map = new mapboxgl.Map({
     container: 'Lab2Map', // map container ID in the index.html file.
-    style: 'mapbox://styles/kevinyuanzy/cm6ztbpqc003s01qwcvdrf6ft', // style URL from created MapBox style.
+    style: 'mapbox://styles/xieyiwe2/cm8c26hyq00a301s58yvi4c93', // style URL from created MapBox style.
     center: [-79.391820, 43.701268], // starting position [lng, lat]. 
     zoom: 11.25, // starting zoom level.
 });
